1368f859e4dac1e0877d3fd06b4d8cdd85af62d6
Allen-Davis-M <138326511+Allen-Davis-M@users.noreply.github.com>
2025-06-11 18:15:48 +0530

    Add files via upload

---
+++ Python_Primer_Problem(R-1.1).py
@@ -0,0 +1,16 @@
+#Chapter-1(Python Primer)
+#Reinforcement
+#R-1.1)
+def is_multiple(n,m):
+    if n%m==0:
+        print("True")
+    else:
+        print("False")
+n=int(input("Enter number here"))
+m=int(input("Enter number here"))
+
+is_multiple(n,m)
+
+
+      
+
